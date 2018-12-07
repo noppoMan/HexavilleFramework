b1c2a6c428722a14dfcf6926e98990329a4ce0e9
noppoman <yuki@miketokyo.com>
2018-12-07 15:51:26 +0900

    fix build error on Travis

--- Scripts/install-swift.sh
+++ Scripts/install-swift.sh
@@ -12,15 +12,17 @@ then
 else
     if [[ $UNAME == "Linux" ]];
     then
-        OS="ubuntu1604";
+        OS="ubuntu1404";
     fi
 fi
 
-if [[ $OS != "macos" ]];
+if [[ $OS == "macos" ]]; 
 then
+    brew install libressl
+else
     sudo apt-get update
     sudo apt-get install -y clang libicu-dev uuid-dev
-    SWIFTFILE="swift-$VERSION-RELEASE-ubuntu16.04";
+    SWIFTFILE="swift-$VERSION-RELEASE-ubuntu14.04";
     wget https://swift.org/builds/swift-$VERSION-release/$OS/swift-$VERSION-RELEASE/$SWIFTFILE.tar.gz
     tar -zxf $SWIFTFILE.tar.gz
     export PATH=$PWD/$SWIFTFILE/usr/bin:"${PATH}"
